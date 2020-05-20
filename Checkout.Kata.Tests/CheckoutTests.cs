fe9bf90572d0db6f970f9380aa275cbeb4db4322
ODYSSEY-I\stephen.banks <stephen.banks@interact-intranet.com>
2020-05-20 16:25:37 +0100

    Update checkout Total tests to include discount and add more tests

--- Checkout.Kata.Tests/CheckoutTests.cs
+++ Checkout.Kata.Tests/CheckoutTests.cs
@@ -17,6 +17,8 @@ namespace Checkout.Kata.Tests
 		[SetUp]
 		public void Setup()
 		{
+			_discountMock = new Mock<IDiscount>();
+
 			_checkout = new Checkout(_discountMock.Object);
 		}
 
@@ -29,21 +31,27 @@ namespace Checkout.Kata.Tests
 		[Test]
 		public void Scan_Item_Negative_Price_Throws_Argument_Exception()
 		{
+			// Arrange
 			var invalidItem = new Item
 			{
 				SKU = "B15",
 				Price = -0.5m
 			};
 
+			// Assert
 			Assert.Throws<ArgumentException>(() => _checkout.Scan(invalidItem));
 		}
 
 		[Test]
 		public void No_Items_Scanned_Total_Is_Zero()
 		{
+			// Act
 			var total = _checkout.Total();
 
+			// Assert
 			Assert.AreEqual(0, total);
+
+			_discountMock.Verify(dm => dm.CalculateDiscountAmount(It.IsAny<IEnumerable<Item>>()), Times.Never);
 		}
 
 		[Test]
@@ -66,7 +74,7 @@ namespace Checkout.Kata.Tests
 		}
 
 		[Test]
-		public void Scan_Multiple_Items_Get_Total_Of_All()
+		public void Scan_Multiple_Items_Get_Total_Of_All_No_Discount()
 		{
 			// Arrange
 			var items = new List<Item>
@@ -88,6 +96,9 @@ namespace Checkout.Kata.Tests
 				}
 			};
 
+			_discountMock.Setup(dm => dm.CalculateDiscountAmount(It.IsAny<IEnumerable<Item>>()))
+				.Returns(0);
+
 			// Act
 			foreach (var item in items)
 			{
@@ -98,6 +109,70 @@ namespace Checkout.Kata.Tests
 
 			// Assert
 			Assert.AreEqual(1.40m, total);
+		}
+
+		[Test]
+		public void Scan_Items_Apply_Single_Discount()
+		{
+			// Arrange
+			var items = new List<Item>
+			{
+				new Item {SKU = "A99", Price = 0.50m},
+				new Item {SKU = "B15", Price = 0.30m},
+				new Item {SKU = "C40", Price = 0.60m},
+				new Item {SKU = "A99", Price = 0.50m},
+				new Item {SKU = "A99", Price = 0.50m}
+			};
+
+			_discountMock.Setup(dm => dm.CalculateDiscountAmount(It.IsAny<IEnumerable<Item>>()))
+				.Returns(0.2m);
+
+			// Act
+			foreach (var item in items)
+			{
+				_checkout.Scan(item);
+			}
+
+			var total = _checkout.Total();
+
+			// Assert
+			Assert.AreEqual(2.2m, total);
+
+			_discountMock.Verify(dm => dm.CalculateDiscountAmount(It.IsAny<IEnumerable<Item>>()), Times.Once);
+		}
+
+		[Test]
+		public void Scan_Items_Apply_Multiple_Discounts()
+		{
+			// Arrange
+			var items = new List<Item>
+			{
+				new Item {SKU = "A99", Price = 0.50m},
+				new Item {SKU = "B15", Price = 0.30m},
+				new Item {SKU = "C40", Price = 0.60m},
+				new Item {SKU = "A99", Price = 0.50m},
+				new Item {SKU = "A99", Price = 0.50m},
+				new Item {SKU = "A99", Price = 0.50m},
+				new Item {SKU = "A99", Price = 0.50m},
+				new Item {SKU = "A99", Price = 0.50m},
+				new Item {SKU = "B15", Price = 0.30m}
+			};
+
+			_discountMock.Setup(dm => dm.CalculateDiscountAmount(It.IsAny<IEnumerable<Item>>()))
+				.Returns(0.55m);
+
+			// Act
+			foreach (var item in items)
+			{
+				_checkout.Scan(item);
+			}
+
+			var total = _checkout.Total();
+
+			// Assert
+			Assert.AreEqual(3.65m, total);
+
+			_discountMock.Verify(dm => dm.CalculateDiscountAmount(It.IsAny<IEnumerable<Item>>()), Times.Once);
 		}
 	}
 }
